d401f02ccf68fcc7440e43d4e7aa24d06401a836
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-01-02 20:06:18 -0800

    Basic snakemake dumper
    This was copied from falcon_unzip. Someday, we need to consolidate.

--- falcon_kit/mains/run1.py
+++ falcon_kit/mains/run1.py
@@ -1,5 +1,5 @@
 from .. import run_support as support
-from .. import bash, pype_tasks
+from .. import bash, pype_tasks, snakemake
 from ..util.system import (only_these_symlinks, lfs_setstripe_maybe)
 # pylint: disable=no-name-in-module, import-error, fixme, line-too-long
 from pypeflow.simple_pwatcher_bridge import (PypeProcWatcherWorkflow, MyFakePypeThreadTaskBase,
@@ -145,13 +145,15 @@ def main1(prog_name, input_config_fn, logger_config_fn=None):
                                  use_tmpdir=config.get('use_tmpdir'),
                                  squash=squash
                                  )
-    run(wf, config,
-        os.path.abspath(input_config_fn),
-        input_fofn_plf=input_fofn_plf,
-        )
+    with open('foo.snake', 'w') as snakemake_writer:
+        rule_writer = snakemake.SnakemakeRuleWriter(snakemake_writer)
+        run(wf, config, rule_writer,
+            os.path.abspath(input_config_fn),
+            input_fofn_plf=input_fofn_plf,
+            )
 
 
-def run(wf, config,
+def run(wf, config, rule_writer,
         input_config_fn,
         input_fofn_plf,
         ):
