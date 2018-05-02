e60d54be97f2aeb67315e6271a54aeb1d373045b
Ivan Sovic <isovic@pacificbiosciences.com>
2018-05-02 08:16:15 -0700

    The reduce_preads.py can now accept more than 1 sg_edges_list files. This will be required for Unzip, where both the 2-asm-falcon and the 3-unzip/1-hasm graphs are used.

--- falcon_kit/mains/reduce_preads.py
+++ falcon_kit/mains/reduce_preads.py
@@ -9,19 +9,33 @@ import argparse
 import sys
 from falcon_kit.FastaReader import open_fasta_reader
 
+default_sg_edges_list_file = ['sg_edges_list']
+
 def run(fp_out, pread_fasta_file, sg_edges_list_file):
+    # Workaround the Argparse issue. It does not override
+    # the default argument value when the parameter is
+    # used in the append mode, but instead adds to the default
+    # list. https://bugs.python.org/issue16399
+    # Instead, we will not specify the default value, and
+    # check if the list is emptu here here, so that the user
+    # can specify exactly the paths to the file(s).
+    if not sg_edges_list_file:
+        sg_edges_list_file = default_sg_edges_list_file
+
     reads_in_layout = set()
-    with open(sg_edges_list_file) as f:
-        for l in f:
-            l = l.strip().split()
-            """001039799:E 000333411:E 000333411 17524 20167 17524 99.62 G"""
-            v, w, rid, s, t, aln_score, idt, type_ = l
-            if type_ != "G":
-                continue
-            r1 = v.split(":")[0]
-            reads_in_layout.add(r1)
-            r2 = w.split(":")[0]
-            reads_in_layout.add(r2)
+
+    for fn in sg_edges_list_file:
+        with open(fn) as fp_in:
+            for l in fp_in:
+                l = l.strip().split()
+                """001039799:E 000333411:E 000333411 17524 20167 17524 99.62 G"""
+                v, w, rid, s, t, aln_score, idt, type_ = l
+                if type_ != "G":
+                    continue
+                r1 = v.split(":")[0]
+                reads_in_layout.add(r1)
+                r2 = w.split(":")[0]
+                reads_in_layout.add(r2)
 
     with open_fasta_reader(pread_fasta_file) as f:
         for r in f:
@@ -34,14 +48,14 @@ def main(argv=sys.argv):
     parser = argparse.ArgumentParser(
             description=description,
             formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--pread_fasta_file', type=str,
+    parser.add_argument('--pread-fasta-file', type=str,
             default='preads4falcon.fasta',
             help='Preads file, required to construct the contigs.')
-    parser.add_argument('--sg_edges_list_file', type=str,
-            default='sg_edges_list',
-            help='File containing string graph edges, produced by ovlp_to_graph.py.')
+    parser.add_argument('--sg-edges-list-file', action='append',
+            help='One or more files containing string graph edges, produced by ovlp_to_graph.py.')
     args = parser.parse_args(argv[1:])
     run(sys.stdout, **vars(args))
 
 if __name__ == "__main__":
     main(sys.argv)
+
