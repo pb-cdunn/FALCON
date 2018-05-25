c5c6cfeb1f9bfd3eb8b4e95d49aa5ff3ecca3959
Ivan Sovic <isovic@pacificbiosciences.com>
2018-05-25 02:13:24 -0700

    Moved the alignment code from graph_to_contig to dedup_a_tigs.

--- falcon_kit/mains/dedup_a_tigs.py
+++ falcon_kit/mains/dedup_a_tigs.py
@@ -4,7 +4,27 @@ from __future__ import print_function
 from falcon_kit.FastaReader import open_fasta_reader
 import argparse
 import sys
+from falcon_kit import kup, falcon, DWA
 
+def get_aln_results(ref_seq, query_seq):
+    # Align the a_ctg against the base.
+    delta_len = len(query_seq) - len(ref_seq)
+    idt = 0.0
+    cov = 0.0
+    if len(ref_seq) > 2000 and len(query_seq) > 2000:
+        try:
+            aln_data = get_aln_data(ref_seq, query_seq)
+            if len(aln_data) != 0:
+                idt = 1.0 - 1.0 * \
+                    aln_data[-1][-1] / aln_data[-1][-2]
+                cov = 1.0 * \
+                    (aln_data[-1][3] - aln_data[-1]
+                        [2]) / aln_data[-1][4]
+        except TooLongError:
+            log('WARNING: Seqs were too long for get_aln_data(), so we set idt/cov low enough to prevent filtering by dedup_a_tigs, at atig_path[:-1] == {}'.format(atig_path[:-1]))
+            idt = -1.0
+            cov = -1.0
+    return delta_len, idt, cov
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(description='Removes duplicate a-tig, iff *all* conditions are violated. Assumes the working directory has the a_ctg_all.fa file, and produces a_ctg.fa',
